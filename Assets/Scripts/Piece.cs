fe306dac04c0cc8f1834bb0d8970fdc8cc1c6473
YUGANT <62373423+YUGANT-LOYA@users.noreply.github.com>
2022-04-05 00:03:45 +0530

    Spawning_MainLogic_Completed

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -7,15 +7,22 @@ public class Piece : MonoBehaviour
     public Vector3Int[] cells { get; private set; }
     public Vector3Int position { get; private set; }
     public int rotationIndex { get; private set; }
+    public float stepDelay = 0.7f;
+    public float lockDelay = 0.5f;
+
+    private float stepTime;
+    private float lockTime;
 
     public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
         this.board = board;
         this.position = position;
         this.data = data;
-
         //This is initialization of the new piece when it spawns in its default space position.
         this.rotationIndex = 0;
+
+        this.stepTime = Time.time + this.stepDelay;
+        this.lockTime = 0f;
 
         //If cells array is null, so we will Initialize the array.
         if (this.cells == null)
@@ -34,6 +41,8 @@ public class Piece : MonoBehaviour
     {
         //Before starting the game, it clear the board.
         this.board.Clear(this);
+        //This will increase with time to keep track of time, when to lock.
+        this.lockTime += Time.deltaTime;
 
         //Input from User/Player.
         if (Input.GetKeyDown(KeyCode.A))
@@ -65,9 +74,34 @@ public class Piece : MonoBehaviour
         {
             RotationTetro(1);
         }
+        //This function will update, and keep track of when the normal time exceeds the stepTime.
+        //and then the Step() function will be called.
+        if(Time.time >= this.stepTime)
+        {
+            Step();
+        }
 
         //This will set the following piece after any of the input taken from user.
         this.board.Set(this);
+    }
+
+    private void Step()
+    {
+        this.stepTime = Time.time + this.stepDelay; 
+        //This will make out Tetro Piece continously move downwards.
+        Move(Vector2Int.down);
+        //This Statement will lock the piece, if the piece is not moving more than lockDelay.
+        if(this.lockTime >= this.lockDelay)
+        {
+            Lock();
+        }
+    }
+    //This will lock the current piece and spawn new piece.
+    private void Lock()
+    {
+        this.board.Set(this);
+        this.board.ClearLines();
+        this.board.SpawnPiece();
     }
 
     //This function will continues to move down until, it reaches it's lowest limit of the board.
@@ -91,6 +125,9 @@ public class Piece : MonoBehaviour
         if (valid)
         {
             this.position = newPosition;
+
+            //This will reset every time whenever there is movement, until it exceeds the lockTime.
+            this.lockTime = 0f;
         }
 
         return valid;
@@ -98,13 +135,14 @@ public class Piece : MonoBehaviour
 
     private void RotationTetro(int direction)
     {
+        //We saved our original rotation which is used to revert the process.
         int originalRotation = this.rotationIndex;
         //For Rotation of a Tetro in 0 to 4 possible ways.
         this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
 
-
         ApplyRotationMatrix(direction);
 
+        //This checks if our rotation fails so it will revert back to its original Rotation.
         if (!TestWallKicks(this.rotationIndex, direction))
         {
             this.rotationIndex = originalRotation;
@@ -144,41 +182,42 @@ public class Piece : MonoBehaviour
             this.cells[i] = new Vector3Int(x, y, 0);
         }
     }
-        private bool TestWallKicks(int rotationIndex, int rotationDirection)
-        {
-            int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
+    //We should know in which RotationIndex we are and in which direction we need to rotate for TestWallKicks.
+    private bool TestWallKicks(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
 
-            for (int i = 0; i < this.data.wallKicks.GetLength(1); i++)
+        for (int i = 0; i < this.data.wallKicks.GetLength(1); i++)
+        {
+            Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
+
+            if (Move(translation))
             {
-                Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
-
-                if (Move(translation))
-                {
-                    return true;
-                }
+                return true;
             }
-            return false;
         }
+        return false;
+    }
+    //There is a pattern in finding WallKickIndex.
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+    {
+        //This is pattern for index, we have to select every alternative. The pattern is on Tetris Wikipedia.   
+        int wallKickIndex = rotationIndex * 2;
 
-        private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+        if (rotationDirection < 0)
         {
-            int wallKickIndex = rotationIndex * 2;
-
-            if (rotationDirection < 0)
-            {
-                wallKickIndex--;
-            }
-
-            return Wrap(wallKickIndex, 0, this.data.wallKicks.GetLength(0));
+            wallKickIndex--;
         }
 
-        //Standard wrap utility maths function, to check whether it is inside the bound or outside the bound.
-        private int Wrap(int input, int min, int max)
-        {
-            if (input < min)
-                return max - (min - input) % (max - min);
-            else
-                return min + (input - min) % (max - min);
-        }
+        return Wrap(wallKickIndex, 0, this.data.wallKicks.GetLength(0));
+    }
 
+    //Standard wrap utility maths function, to check whether it is inside the bound or outside the bound.
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+            return max - (min - input) % (max - min);
+        else
+            return min + (input - min) % (max - min);
+    }
 }
